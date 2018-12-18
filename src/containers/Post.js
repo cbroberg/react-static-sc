66c0a15f9c24141b66b78a93200b733cc28a0599
Christian Broberg <cb@cb-pro.local>
2018-12-18 23:02:20 +0100

    Repo ready with image of error

--- src/containers/Post.js
+++ src/containers/Post.js
@@ -1,13 +1,17 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import { withRouteData } from 'react-static'
 import { Link } from '@reach/router'
-//
+import { Parser } from 'html-to-react'
+
+const htmlToReactParser = new Parser()
 
 export default withRouteData(({ post }) => (
-  <div>
-    <Link to="/blog/">{'<'} Back</Link>
-    <br />
-    <h3>{post.title}</h3>
-    <p>{post.content}</p>
-  </div>
+	<div>
+		<Link to="/blog/">{'<'} Back</Link>
+		<br />
+		{/* <h3>{post.title}</h3> */}
+		<p>{htmlToReactParser.parse(post.content)}</p>
+	</div>
 ))
+
